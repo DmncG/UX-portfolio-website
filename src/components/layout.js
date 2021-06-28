2cbf2c59038e6d74232ec8c6a734bfab7d0c3b74
strandbooks <dom@strandbooks.com>
2021-06-28 17:20:11 -0400

    added test blog files

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,28 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
+
+const GET_TITLE = graphql`
+  query{
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(GET_TITLE);
+
   return (
     <main>
-      <title>{pageTitle}</title>
+      <title>{pageTitle} | {data?.site?.siteMetadata?.title}</title>
+      <p>{data?.site?.siteMetadata?.title}</p>
       <nav>
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
         </ul>
       </nav>
       <h1>{pageTitle}</h1>
