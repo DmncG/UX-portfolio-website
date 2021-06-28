a94c37153416e10f10f265245def75011864e466
strandbooks <dom@strandbooks.com>
2021-06-28 16:52:35 -0400

    gatsby tutorial

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const About = () => (
+  <Layout pageTitle="About Me">
+    <p>this is me in all my glory bwahahaha</p>
+  </Layout>
+)
+
+export default About;
