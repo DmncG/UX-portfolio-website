2bf7b98b1694adbecc9a2c3d2e9ac05b8a01a59b
strandbooks <dom@strandbooks.com>
2021-07-01 17:48:41 -0400

    added tsparticles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,20 @@
-import * as React from "react";
-import Layout from '../components/layout';
-import { StaticImage } from "gatsby-plugin-image";
+import * as React from "react"
+import Layout from '../components/layout'
+import Profile from '../components/home/profile'
+import { StaticImage } from "gatsby-plugin-image"
 
 // markup
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      {/* <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
         src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
-      />
+      /> */}
+      <div className="w-screen">
+      <Profile />
+      </div>
     </Layout>
   )
 }
