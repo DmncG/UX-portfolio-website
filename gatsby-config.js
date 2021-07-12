53927db7f7394db1bc3a9d6429b5307b865958b8
strandbooks <dom@strandbooks.com>
2021-07-12 17:41:41 -0400

    updated analytics plugin placement in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,27 +3,6 @@ module.exports = {
     title: "UX-portfolio-website",
   },
   plugins: [
-    "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-postcss",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: `blog`,
-        path: `${__dirname}/blog`,
-      }
-    },
-    {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [
-          `jost\:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i`,
-          `poppins\:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i` // you can also specify font weights and styles
-        ],
-        display: 'swap'
-      }
-    },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
@@ -54,6 +33,27 @@ module.exports = {
         // defaults to false
         enableWebVitalsTracking: true,
       },
+    },
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-postcss",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `jost\:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i`,
+          `poppins\:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i` // you can also specify font weights and styles
+        ],
+        display: 'swap'
+      }
     },
   ],
 };
