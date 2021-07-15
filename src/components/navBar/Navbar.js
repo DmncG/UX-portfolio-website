cc2d5ff5c09841777823fdc8d3e15f4dc31ff2a7
DmncG <micksdev@gmail.com>
2021-07-15 01:02:34 -0400

    created Navbar scaffolding; updated About page

---
+++ src/components/navBar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      This is a navbar. I have to put links here and make it responsive.
+    </div>
+  )
+}
+
+export default Navbar
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import Layout from '../components/layout';
+import React from 'react'
+import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const About = () => (
   <Layout pageTitle="About">
-    About Me
+    <div className="w-screen h-screen bg-gunPowder p-4">
+      <div className="overflow-hidden rounded-full w-60 h-60 flex justify-center mx-auto border-4 border-terracota">
+        <StaticImage src="../images/profile.jpg" alt="profile" />
+      </div>
+      <div>
+        <h1 className="font-display text-4xl my-4 font-semibold text-rice">About</h1>
+        <div>
+          <p className="font-body font-normal text-md text-rice">
+            Hi. My name is Dom.
+            <br />
+            <br />
+            I'm a UX Designer/Fullstack Engineer based in New York City.
+            <br />
+            <br />
+            {/* Currently, I'm working on a building a Shopify-based ecommerce website that sells a wide variety of wood products */}
+          </p>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
