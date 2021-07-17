227eb2dd8694d1a7dbab58f97086dc0198f62ad6
DmncG <micksdev@gmail.com>
2021-07-17 19:30:17 -0400

    updated Button h in lg screens

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby'
 const Button = ({ label, type, shape, size, link, onClick, color }) => {
   return (
     <Link to={link}>
-        <div className={`shadow-md hover:shadow-xl rounded-full w-36 md:w-40 lg:w-52 xl:w-60 h-36 md:h-40 lg:h-48 xl:h-60 flex justify-center items-center bg-${color} hover:bg-${color}-light active:animate-pulse`}>
+        <div className={`shadow-md hover:shadow-xl rounded-full w-36 md:w-40 lg:w-52 xl:w-60 h-36 md:h-40 lg:h-52 xl:h-60 flex justify-center items-center bg-${color} hover:bg-${color}-light active:animate-pulse`}>
         <p className="font-display font-semibold text-center text-2xl md:text-3xl xl:text-4xl text-rice">{label}</p>
         </div>
     </Link>
