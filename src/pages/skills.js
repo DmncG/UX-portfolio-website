b366b2d4a2575447a40106ccf2f2d972395c5901
strandbooks <dom@strandbooks.com>
2021-07-20 15:00:08 -0400

    added skills page

---
+++ src/pages/skills.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+
+const Skills = ({ location }) => (
+  <Layout pageTitle="Skills" location={location}>
+    <div className="w-screen h-screen bg-gunPowder p-4">
+      <p>This is the Skills page</p>
+    </div>
+  </Layout>
+)
+
+export default Skills;
