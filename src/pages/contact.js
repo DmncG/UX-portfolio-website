00f38af16081e4b01f6ec0172c904881f7c2c237
DmncG <micksdev@gmail.com>
2021-07-24 13:03:53 -0400

    added return to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const Contact = () => {
-  <div>
-    <p>This is my contact page</p>
-  </div>
+    return (
+    <div>
+      <p>This is my contact page</p>
+    </div>
+    )
 }
 
 export default Contact
