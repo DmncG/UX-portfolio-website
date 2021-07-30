7a05f3a6af8c5d48824ce4d85ec8e71c6322abb5
strandbooks <dom@strandbooks.com>
2021-07-30 11:39:55 -0400

    added SkillsList

---
+++ src/components/skills/SkillsList.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import ExpandLessRoundedIcon from '@material-ui/icons/ExpandLessRounded'
+import ExpandMoreRoundedIcon from '@material-ui/icons/ExpandMoreRounded'
+import FactCheckRoundedIcon from '@material-ui/icons/FactCheckRounded'
+import LaptopRoundedIcon from '@material-ui/icons/LaptopRounded'
+import LocalPhoneRoundedIcon from '@material-ui/icons/LocalPhoneRounded'
+import PhoneIphoneRoundedIcon from '@material-ui/icons/PhoneIphoneRounded'
+import SettingsRoundedIcon from '@material-ui/icons/SettingsRounded'
+import TabletRoundedIcon from '@material-ui/icons/TabletRounded'
+import WbIncandescentRoundedIcon from '@material-ui/icons/WbIncandescentRounded'
+import BuildRoundedIcon from '@material-ui/icons/BuildRounded'
+import ArtTrackRoundedIcon from '@material-ui/icons/ArtTrackRounded'
+
+const SkillsList = () => {
+  return (
+    <div>
+      <div>
+        <section>
+          <div>
+            <div>
+              <FactCheckRoundedIcon />
+              <LocalPhoneRoundedIcon />
+              <ArtTrackRoundedIcon />
+            </div>
+            <h1>Research</h1>
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <div>
+              <WbIncandescentRoundedIcon />
+            </div>
+            <h1>Ideation</h1>
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <div>
+              <LaptopRoundedIcon />
+              <PhoneIphoneRoundedIcon />
+              <TabletRoundedIcon />
+            </div>
+            <h1>Mockups & Prototyping</h1>
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <div>
+              <SettingsRoundedIcon />
+              <BuildRoundedIcon />
+            </div>
+            <h1>Development</h1>
+          </div>
+        </section>
+      </div>
+    </div>
+  )
+}
+
+export default SkillsList
+
+
+
+
--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Layout from '../components/layout'
-import { StaticImage } from 'gatsby-plugin-image'
+import SkillsList from '../components/skills/SkillsList'
 
 const Skills = ({ location }) => (
   <Layout pageTitle="Skills" location={location}>
     <div className="w-screen h-screen bg-padua p-4">
-      <p>This is the Skills page</p>
+      <SkillsList />
     </div>
   </Layout>
 )
