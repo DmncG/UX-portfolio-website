7860d09ad1dabefe74e4d1030c03b16ddf466be3
strandbooks <dom@strandbooks.com>
2021-08-12 17:21:06 -0400

    adding content in MoreArt; added user persona assets

--- src/components/projects/MoreArt.js
+++ src/components/projects/MoreArt.js
@@ -26,6 +26,46 @@ const MoreArt = () => (
       </div> 
       <Header level={1}>MoreArt</Header>
       <Header level={2}>Redesigning <strong>MoreArt</strong>, an organization that supports collaboration between artists and communities to create public art</Header>
+
+      <div>
+        <section>
+          <Header level={1}>Overview</Header>
+          <div>
+            <div className="py-2 px-4 my-2">
+              <h2 className="font-display font-semibold text-lg my-2">Background</h2>
+              <p className="font-body text-base leading-relaxed">More Art is a 501(c)(3) nonprofit organization that supports collaborations between professional artists and communities to create public art and educational programs that inspire social justice.</p>
+            </div>
+
+            <div className="py-2 px-4 my-2">
+              <h2 className="font-display font-semibold text-lg my-2">Objectives</h2>
+              <p>To redesign MoreArt.org's website by applying the design thinking process</p>
+            </div>
+
+            <div className="py-2 px-4 my-2">
+              <h2 className="font-display font-semibold text-lg my-2">My Role</h2>
+              <p>UX Designer, team of 3</p>
+            </div>
+
+            <div className="py-2 px-4 my-2">
+              <h2 className="font-display font-semibold text-lg my-2">Timeline</h2>
+              <p>Two weeks</p>
+            </div>
+
+            <div className="py-2 px-4 my-2">
+              <h2 className="font-display font-semibold text-lg my-2">Tools</h2>
+              <p>Figma, Miro, Zoom, Otter.ai, Previewed</p>
+            </div>
+          </div>
+        </section>
+
+        <section>
+
+        </section>
+
+        <section>
+
+        </section>
+      </div>
     </div>
   </>
 )
