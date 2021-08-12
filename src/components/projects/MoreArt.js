81acbb4d59e597373c6779040892156acd75c835
strandbooks <dom@strandbooks.com>
2021-08-12 17:57:24 -0400

    added survey portion

--- src/components/projects/MoreArt.js
+++ src/components/projects/MoreArt.js
@@ -32,27 +32,27 @@ const MoreArt = () => (
           <Header level={1}>Overview</Header>
           <div className="md:grid md:grid-cols-2 md:justify-center md:items-start">
             <div className="py-2 px-4 my-2">
-              <h2 className="font-display font-semibold text-2xl my-2">Background</h2>
+              <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">Background</h2>
               <p className="font-body text-base leading-relaxed">More Art is a 501(c)(3) nonprofit organization that supports collaborations between professional artists and communities to create public art and educational programs that inspire social justice.</p>
             </div>
 
             <div className="py-2 px-4 my-2">
-              <h2 className="font-display font-semibold text-2xl my-2">Objectives</h2>
+              <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">Objectives</h2>
               <p className="font-body text-base leading-relaxed">To redesign MoreArt.org's website by applying the design thinking process</p>
             </div>
 
             <div className="py-2 px-4 my-2">
-              <h2 className="font-display font-semibold text-2xl my-2">My Role</h2>
+              <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">My Role</h2>
               <p className="font-body text-base leading-relaxed">UX Designer, team of 3</p>
             </div>
 
             <div className="py-2 px-4 my-2">
-              <h2 className="font-display font-semibold text-2xl my-2">Timeline</h2>
+              <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">Timeline</h2>
               <p className="font-body text-base leading-relaxed">Two weeks</p>
             </div>
 
             <div className="py-2 px-4 my-2">
-              <h2 className="font-display font-semibold text-2xl my-2">Tools</h2>
+              <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">Tools</h2>
               <p className="font-body text-base leading-relaxed">Figma, Miro, Zoom, Otter.ai, Previewed</p>
             </div>
           </div>
@@ -61,17 +61,26 @@ const MoreArt = () => (
         <section>
           <Header level={1}>Research</Header>
           <div>
-            <div className="py-2 px-4 my-2">
-              <h2 className="font-display font-semibold text-2xl my-2">Research Interviews</h2>
+            <div className="py-2 px-4 my-4">
+              <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">Research Interviews</h2>
               <p className="font-body text-base leading-relaxed">We began the process by conducting 5 research interviews to figure out the habits and painpoints of users when they donate online. Moreover, we wanted to know the general needs of people who are interested in social initiatives, and get a sense of how they perceive non-profit organizations as a whole.</p>
-              <div className="grid grid-cols-2 justify-center my-8">
+              <div className="grid grid-cols-2 justify-center my-8 lg:max-w-screen-md mx-auto">
                 <StaticImage src="../../images/moreart-interview-1.png" alt="research interview 1" />
 
-                <StaticImage src="../../images/moreart-interview-2.png" alt="research interview 1" />
+                <StaticImage src="../../images/moreart-interview-2.png" alt="research interview 2" />
 
-                <StaticImage src="../../images/moreart-interview-3.png" alt="research interview 1" />
+                <StaticImage src="../../images/moreart-interview-3.png" alt="research interview 3" />
 
-                <StaticImage src="../../images/moreart-interview-4.png" alt="research interview 1" />
+                <StaticImage src="../../images/moreart-interview-4.png" alt="research interview 4" />
+              </div>
+              <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">Survey</h2>
+              <div className="grid grid-cols-2 items-center">
+                <p className="font-display text-9xl text-gunPowder">86.4%</p>
+                <div>
+                  <p className="font-body text-base leading-relaxed">
+                    We also conducted a survey to get some quantitative data regarding this subject. Out of 22 responses, 86.4% wanted to know where their donations went.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
