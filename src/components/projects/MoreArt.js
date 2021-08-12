d3d98c2269947c66f3d5db40e07b4aac7f6505a3
strandbooks <dom@strandbooks.com>
2021-08-12 17:46:28 -0400

    adding content to moreart

--- src/components/projects/MoreArt.js
+++ src/components/projects/MoreArt.js
@@ -4,7 +4,7 @@ import Header from '../header/Header'
 
 const MoreArt = () => (
   <>
-    <div className="max-w-screen-lg lg:h-screen md:mx-auto py-16">
+    <div className="max-w-screen-md xl:max-w-screen-lg md:mx-auto py-16">
       <div className="mt-16 mb-16">
         <div className="relative flex flex-col justify-center items-center max-w-sm sm:mx-auto">
           <StaticImage
@@ -30,40 +30,63 @@ const MoreArt = () => (
       <div>
         <section>
           <Header level={1}>Overview</Header>
-          <div>
+          <div className="md:grid md:grid-cols-2 md:justify-center md:items-start">
             <div className="py-2 px-4 my-2">
-              <h2 className="font-display font-semibold text-lg my-2">Background</h2>
+              <h2 className="font-display font-semibold text-2xl my-2">Background</h2>
               <p className="font-body text-base leading-relaxed">More Art is a 501(c)(3) nonprofit organization that supports collaborations between professional artists and communities to create public art and educational programs that inspire social justice.</p>
             </div>
 
             <div className="py-2 px-4 my-2">
-              <h2 className="font-display font-semibold text-lg my-2">Objectives</h2>
-              <p>To redesign MoreArt.org's website by applying the design thinking process</p>
+              <h2 className="font-display font-semibold text-2xl my-2">Objectives</h2>
+              <p className="font-body text-base leading-relaxed">To redesign MoreArt.org's website by applying the design thinking process</p>
             </div>
 
             <div className="py-2 px-4 my-2">
-              <h2 className="font-display font-semibold text-lg my-2">My Role</h2>
-              <p>UX Designer, team of 3</p>
+              <h2 className="font-display font-semibold text-2xl my-2">My Role</h2>
+              <p className="font-body text-base leading-relaxed">UX Designer, team of 3</p>
             </div>
 
             <div className="py-2 px-4 my-2">
-              <h2 className="font-display font-semibold text-lg my-2">Timeline</h2>
-              <p>Two weeks</p>
+              <h2 className="font-display font-semibold text-2xl my-2">Timeline</h2>
+              <p className="font-body text-base leading-relaxed">Two weeks</p>
             </div>
 
             <div className="py-2 px-4 my-2">
-              <h2 className="font-display font-semibold text-lg my-2">Tools</h2>
-              <p>Figma, Miro, Zoom, Otter.ai, Previewed</p>
+              <h2 className="font-display font-semibold text-2xl my-2">Tools</h2>
+              <p className="font-body text-base leading-relaxed">Figma, Miro, Zoom, Otter.ai, Previewed</p>
             </div>
           </div>
         </section>
 
         <section>
+          <Header level={1}>Research</Header>
+          <div>
+            <div className="py-2 px-4 my-2">
+              <h2 className="font-display font-semibold text-2xl my-2">Research Interviews</h2>
+              <p className="font-body text-base leading-relaxed">We began the process by conducting 5 research interviews to figure out the habits and painpoints of users when they donate online. Moreover, we wanted to know the general needs of people who are interested in social initiatives, and get a sense of how they perceive non-profit organizations as a whole.</p>
+              <div className="grid grid-cols-2 justify-center my-8">
+                <StaticImage src="../../images/moreart-interview-1.png" alt="research interview 1" />
 
+                <StaticImage src="../../images/moreart-interview-2.png" alt="research interview 1" />
+
+                <StaticImage src="../../images/moreart-interview-3.png" alt="research interview 1" />
+
+                <StaticImage src="../../images/moreart-interview-4.png" alt="research interview 1" />
+              </div>
+            </div>
+          </div>
         </section>
 
         <section>
+          <Header level={1}>Definition & Ideation</Header>
+        </section>
 
+        <section>
+          <Header level={1}>Prototyping & Testing</Header>
+        </section>
+
+        <section>
+          <Header level={1}>Next Steps</Header>
         </section>
       </div>
     </div>
