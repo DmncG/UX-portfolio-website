08886308a12b5b48be204f9abcb929967aaa855e
strandbooks <dom@strandbooks.com>
2021-08-13 14:03:11 -0400

    added moreart assets; updated moreart research section

--- src/components/projects/MoreArt.js
+++ src/components/projects/MoreArt.js
@@ -32,27 +32,42 @@ const MoreArt = () => (
           <Header level={1}>Overview</Header>
           <div className="md:grid md:grid-cols-2 md:justify-center md:items-start">
             <div className="py-2 px-4 my-2">
-              <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">Background</h2>
+              <div>
+                <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">Background</h2>
+                <div className="w-8 bg-terracota mt-2 mb-4 h-1" />
+              </div>
               <p className="font-body text-base leading-relaxed">More Art is a 501(c)(3) nonprofit organization that supports collaborations between professional artists and communities to create public art and educational programs that inspire social justice.</p>
             </div>
 
             <div className="py-2 px-4 my-2">
-              <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">Objectives</h2>
+              <div>
+                <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">Objectives</h2>
+                <div className="w-8 bg-terracota mt-2 mb-4 h-1" />
+              </div>
               <p className="font-body text-base leading-relaxed">To redesign MoreArt.org's website by applying the design thinking process</p>
             </div>
 
             <div className="py-2 px-4 my-2">
-              <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">My Role</h2>
+              <div>
+                <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">My Role</h2>
+                <div className="w-8 bg-terracota mt-2 mb-4 h-1" />
+              </div>
               <p className="font-body text-base leading-relaxed">UX Designer, team of 3</p>
             </div>
 
             <div className="py-2 px-4 my-2">
-              <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">Timeline</h2>
+              <div>
+                <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">Timeline</h2>
+                <div className="w-8 bg-terracota mt-2 mb-4 h-1" />
+              </div>
               <p className="font-body text-base leading-relaxed">Two weeks</p>
             </div>
 
             <div className="py-2 px-4 my-2">
-              <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">Tools</h2>
+              <div>
+                <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">Tools</h2>
+              <div className="w-8 bg-terracota mt-2 mb-4 h-1" />
+              </div>
               <p className="font-body text-base leading-relaxed">Figma, Miro, Zoom, Otter.ai, Previewed</p>
             </div>
           </div>
@@ -62,26 +77,147 @@ const MoreArt = () => (
           <Header level={1}>Research</Header>
           <div>
             <div className="py-2 px-4 my-4">
-              <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">Research Interviews</h2>
-              <p className="font-body text-base leading-relaxed">We began the process by conducting 5 research interviews to figure out the habits and painpoints of users when they donate online. Moreover, we wanted to know the general needs of people who are interested in social initiatives, and get a sense of how they perceive non-profit organizations as a whole.</p>
-              <div className="grid grid-cols-2 justify-center my-8 lg:max-w-screen-md mx-auto">
-                <StaticImage src="../../images/moreart-interview-1.png" alt="research interview 1" />
 
-                <StaticImage src="../../images/moreart-interview-2.png" alt="research interview 2" />
-
-                <StaticImage src="../../images/moreart-interview-3.png" alt="research interview 3" />
-
-                <StaticImage src="../../images/moreart-interview-4.png" alt="research interview 4" />
-              </div>
-              <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">Survey</h2>
-              <div className="grid grid-cols-2 items-center">
-                <p className="font-display text-9xl text-gunPowder">86.4%</p>
+              <div className="my-4">
                 <div>
-                  <p className="font-body text-base leading-relaxed">
-                    We also conducted a survey to get some quantitative data regarding this subject. Out of 22 responses, 86.4% wanted to know where their donations went.
+                  <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">Research Interviews</h2>
+                  <div className="w-8 bg-terracota mt-2 mb-4 h-1" />
+                </div>
+                <p className="font-body text-base leading-relaxed">We began the process by conducting 4 research interviews to <strong>figure out the habits and painpoints of users when they donate online</strong>. Moreover, we wanted to know the general needs of people who are interested in social initiatives, and get a sense of how they perceive non-profit organizations as a whole.</p>
+                <div className="grid grid-cols-2 justify-center my-16 lg:max-w-screen-md mx-auto">
+                  <StaticImage src="../../images/moreart-interview-1.png" alt="research interview 1" />
+
+                  <StaticImage src="../../images/moreart-interview-2.png" alt="research interview 2" />
+
+                  <StaticImage src="../../images/moreart-interview-3.png" alt="research interview 3" />
+
+                  <StaticImage src="../../images/moreart-interview-4.png" alt="research interview 4" />
+                </div>
+              </div>
+
+              <div className="my-4">
+                <div>
+                  <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">Research Findings</h2>
+                  <div className="w-8 bg-terracota mt-2 mb-4 h-1" />
+                </div>
+                <ul className="list-disc pl-8">
+                  <li className="font-body text-xl my-4">
+                    There is a desire to see people genuinely help others
+                  </li>
+                  <li className="font-body text-xl my-4">
+                    There is a general agreement that it's important to support social causes
+                  </li>
+                  <li className="font-body text-xl my-4">
+                    They discover non-profit organizations and social causes via social media
+                  </li>
+                </ul>
+                <div className="my-16">
+                  <p className="font-body text-gunPowder font-semibold text-lg sm:text-xl md:text-3xl leading-relaxed my-2">
+                    “In an age where everything is driven by profit, non-profit organizations believe in a cause and I want to help”
                   </p>
                 </div>
               </div>
+
+              <div className="my-4">
+                <div>
+                  <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">Survey</h2>
+                  <div className="w-8 bg-terracota mt-2 mb-4 h-1" />
+                </div>
+                <div className="grid grid-cols-2 items-center my-8 lg:my-12 mx-auto max-w-screen-sm lg:max-w-screen-md">
+                  <p className="font-display text-center text-4xl md:text-7xl lg:text-8xl text-padua">86.4%</p>
+                  <div>
+                    <p className="font-body text-base leading-relaxed lg:text-xl">
+                      We also conducted a survey to get some quantitative data regarding this subject. Out of 22 responses, 86.4% wanted to know where their donations went.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="my-4">
+                <h2 className="font-display font-semibold text-2xl text-gunPowder my-2 relative">What makes it difficult to donate?</h2>
+
+                <div className="my-16 flex flex-col">
+                  <p className="font-body font-semibold text-lg sm:text-xl md:text-3xl leading-relaxed my-2">
+                    "Not knowing where the donation will end up"
+                  </p>
+                  <p className="font-body text-xl md:text-2xl leading-relaxed my-2 md:my-8 self-end relative right-2 md:right-40">
+                    "It's not convenient"
+                  </p>
+                  <p className="font-body font-bold text-xl md:text-5xl leading-relaxed my-2 relative left-6">
+                    "Lack of trust"
+                  </p>
+                </div>
+              </div>
+
+              <div className="my-4">
+                <div>
+                  <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">Proto Persona</h2>
+                  <div className="w-8 bg-terracota mt-2 mb-4 h-1" />
+                </div>
+                <div className="my-8 flex flex-col">
+                  <p className="font-body text-base leading-relaxed">We created a proto persona to represent our assumptions of MoreArt's ideal user</p>
+                  <StaticImage
+                    src="../../images/moreart-proto-persona.jpg"
+                    alt="proto persona"
+                    className="my-8 self-center"
+                  />
+                </div>
+              </div>
+
+              <div className="my-4">
+                <div>
+                  <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">Heuristic Evaluation</h2>
+                  <div className="w-8 bg-terracota mt-2 mb-4 h-1" />
+                </div>
+                <div className="my-8 flex flex-col">
+                  <p className="font-body text-base leading-relaxed">After performing a heuristic evaluation on MoreArt's current website, it was evident that there was a need to <strong>make the designs between pages feel more consistent</strong>. There were also a number of broken links throughout the site. Despite all this, the website does have help available in every page and there are a limited number of buttons and links.</p>
+                  <div className="my-16 lg:max-w-screen-md mx-auto grid grid-cols-3">
+                    <StaticImage
+                      src="../../images/moreart-heuristic-1.png"
+                      alt="Heuristic 1"
+                      className="my-8 self-center"
+                    />
+                    <StaticImage
+                      src="../../images/moreart-heuristic-2.png"
+                      alt="Heuristic 2"
+                      className="my-8 self-center"
+                    />
+                    <StaticImage
+                      src="../../images/moreart-heuristic-3.png"
+                      alt="Heuristic 3"
+                      className="my-8 self-center"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="my-4">
+                <div>
+                  <h2 className="font-display font-semibold text-2xl text-gunPowder my-2">Annotations</h2>
+                  <div className="w-8 bg-terracota mt-2 mb-4 h-1" />
+                </div>
+                <div className="my-8 flex flex-col">
+                  <p className="font-body text-base leading-relaxed">After performing a heuristic evaluation on MoreArt's current website, it was evident that there was a need to <strong>make the designs between pages feel more consistent</strong>. There were also a number of broken links throughout the site. Despite all this, the website does have help available in every page and there are a limited number of buttons and links.</p>
+                  <div className="my-16 lg:max-w-screen-md mx-auto grid grid-cols-3">
+                    <StaticImage
+                      src="../../images/moreart-anno-1.png"
+                      alt="Annotations 1"
+                      className="my-8 self-center"
+                    />
+                    <StaticImage
+                      src="../../images/moreart-anno-2.png"
+                      alt="Annotations 2"
+                      className="my-8 self-center"
+                    />
+                    <StaticImage
+                      src="../../images/moreart-anno-3.png"
+                      alt="Annotations 3"
+                      className="my-8 self-center"
+                    />
+                  </div>
+                </div>
+              </div>
+
             </div>
           </div>
         </section>
