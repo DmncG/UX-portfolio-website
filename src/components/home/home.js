bb4049d448feeed8cd78834aab57856fcb09aa21
DmncG <micksdev@gmail.com>
2021-12-01 14:20:00 -0500

    updated resume

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,7 +4,7 @@ import { StaticImage } from 'gatsby-plugin-image'
 import ArrowDropDownCircleOutlinedIcon from '@material-ui/icons/ArrowDropDownCircleOutlined';
 import { motion } from 'framer-motion'
 import Button from '../button/Button'
-import pdfCV from '../../downloads/CV_Dominic_Garcia_9_10_2021.pdf'
+import pdfCV from '../../downloads/CV_Dominic_Garcia_12_01_2021.pdf'
 import ParticlePreset from '../particles/ParticlePreset'
 
 const sentence = {
